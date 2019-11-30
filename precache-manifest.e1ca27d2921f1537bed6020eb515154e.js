5ace3254b039e1d4d41283fb66b7e8decd4fc46b
danilokujacic <danilo.kujacic01@gmail.com>
2019-11-30 10:50:58 +0100

    Updates

---
+++ precache-manifest.e1ca27d2921f1537bed6020eb515154e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821602eaeba6aba6625d1610f0d86757",
+    "url": "/firstreact/index.html"
+  },
+  {
+    "revision": "5a4f4fecd6db977080b2",
+    "url": "/firstreact/static/css/main.40dca001.chunk.css"
+  },
+  {
+    "revision": "efcd72e8e2a3c8cad152",
+    "url": "/firstreact/static/js/2.ed369ddd.chunk.js"
+  },
+  {
+    "revision": "5a4f4fecd6db977080b2",
+    "url": "/firstreact/static/js/main.477b2dd2.chunk.js"
+  },
+  {
+    "revision": "9fc687d52c37682a6ebb",
+    "url": "/firstreact/static/js/runtime-main.216a7de5.js"
+  }
+]);
