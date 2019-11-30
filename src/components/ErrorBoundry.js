8749e002dd44d46046da8bf371eb5446dfbe79d1
danilokujacic <danilo.kujacic01@gmail.com>
2019-11-30 17:55:00 +0100

    ff

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class ErrorBoundry {
+export default class ErrorBoundry extends Component{
   constructor(props) {
     super(props);
     this.state = {
